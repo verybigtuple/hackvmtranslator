27664410e3337168e8129555a1512533c5a8d75c
verybigtuple <bigtuple@gmail.com>
2021-02-14 19:24:46 +0300

    Added flags for cmdline interface

--- translator.go
+++ translator.go
@@ -3,53 +3,95 @@ package main
 import (
 	"bufio"
 	"errors"
+	"flag"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-func main() {
-	if len(os.Args) < 3 {
-		panic("Not enough args")
-	}
+func parseCmdline() (inFilePath string, outFilePath string, err error) {
+	inFileFlag := flag.String("in", "", "Input file. Usually has the extension '.vm'")
+	outFileFlag := flag.String("out", "", "Output file. Usually has the extension '.asm'")
+	flag.Parse()
 
-	inFilePath := os.Args[1]
-	inFile, err := os.Open(inFilePath)
-	if err != nil {
-		panic("No File found")
-	}
-	defer inFile.Close()
-	inReader := bufio.NewReader(inFile)
-
-	outFile, err := os.Create(os.Args[2])
-	if err != nil {
-		panic("Cannot create output file")
-	}
-	defer func() {
-		err := outFile.Close()
-		if err != nil {
-			panic("Cannot close output file")
+	inFilePath = *inFileFlag
+	if inFilePath == "" {
+		if flag.Arg(0) == "" {
+			err = fmt.Errorf("Input file is not set")
+			return
 		}
-	}()
-	outWriter := bufio.NewWriter(outFile)
+		inFilePath = flag.Arg(0)
+	}
 
-	stPrefix := strings.TrimSuffix(filepath.Base(inFilePath), filepath.Ext(inFilePath))
+	outFilePath = *outFileFlag
+	if outFilePath == "" {
+		if flag.Arg(1) == "" {
+			fn := strings.TrimSuffix(filepath.Base(inFilePath), filepath.Ext(inFilePath))
+			asmFn := fn + ".asm"
+			outFilePath = filepath.Join(filepath.Dir(inFilePath), asmFn)
+		} else {
+			outFilePath = flag.Arg(1)
+		}
+	}
+
+	return
+}
+
+func run(stPrefix string, inReader *bufio.Reader, outWriter *bufio.Writer) error {
 	parser := NewParser(inReader)
 	codeWr := NewCodeWriter(stPrefix, outWriter)
 	for {
 		cmd, err := parser.ParseNext()
-		// println(cmd)
 		if errors.Is(err, io.EOF) {
 			break
 		}
 		if err != nil {
-			panic(err)
+			return err
 		}
 		if err := codeWr.WriteCommand(*cmd); err != nil {
-			panic(err)
+			return err
 		}
 	}
-	outWriter.Flush()
-	outFile.Sync()
+	err := outWriter.Flush()
+	return err
+}
+
+func main() {
+	inFilePath, outFilePath, err := parseCmdline()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("Argument Error: %v", err))
+		os.Exit(1)
+	}
+
+	inFile, err := os.Open(inFilePath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("Cannot open input file: %v", err))
+		os.Exit(2)
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(outFilePath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("Cannot create output file: %v", err))
+		os.Exit(2)
+	}
+	defer func() {
+		err := outFile.Close()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, fmt.Sprintf("Cannot close output file: %v", err))
+			os.Exit(2)
+		}
+	}()
+
+	inReader := bufio.NewReader(inFile)
+	outWriter := bufio.NewWriter(outFile)
+	stPrefix := strings.TrimSuffix(filepath.Base(inFilePath), filepath.Ext(inFilePath))
+	err = run(stPrefix, inReader, outWriter)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("Cannot create output file: %v", err))
+		os.Exit(3)
+	}
+	fmt.Printf("Asm file saved as %v\n", outFilePath)
 }
